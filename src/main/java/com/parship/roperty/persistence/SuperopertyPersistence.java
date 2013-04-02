a63b5b471a4c9548c2231edb95a4f411dc4303d0
Malte Finsterwalder <malte.finsterwalder@parship.com>
2013-04-02 15:29:14 +0200

    failing loadAll test

--- src/main/java/com/parship/roperty/persistence/SuperopertyPersistence.java
+++ src/main/java/com/parship/roperty/persistence/SuperopertyPersistence.java
@@ -1,8 +1,19 @@
 package com.parship.roperty.persistence;
 
+import com.parship.roperty.Roperty;
+
+
 /**
  * @author mfinsterwalder
  * @since 2013-04-02 15:16
  */
 public class SuperopertyPersistence {
+	private final Roperty roperty;
+
+	public SuperopertyPersistence(final Roperty roperty) {
+		this.roperty = roperty;
+	}
+
+	public void loadAll() {
+	}
 }
--- src/test/java/com/parship/roperty/persistence/SuperopertyPersistenceTest.java
+++ src/test/java/com/parship/roperty/persistence/SuperopertyPersistenceTest.java
@@ -1,8 +1,23 @@
 package com.parship.roperty.persistence;
 
+import com.parship.roperty.Roperty;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
 /**
  * @author mfinsterwalder
  * @since 2013-04-02 15:15
  */
 public class SuperopertyPersistenceTest {
+
+	@Test
+	public void init() {
+		Roperty roperty = new Roperty();
+		SuperopertyPersistence persistence = new SuperopertyPersistence(roperty);
+		persistence.loadAll();
+		assertThat((String)roperty.get("key"), is("value"));
+	}
 }
