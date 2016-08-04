33868d89b6099a5bf27ae39623a763f820550d76
Daniel Heid <dheid@posteo.de>
2016-08-04 08:43:43 +0200

    map will never be null, so statement could be removed

--- src/main/java/com/parship/roperty/MapBackedDomainResolver.java
+++ src/main/java/com/parship/roperty/MapBackedDomainResolver.java
@@ -78,7 +78,7 @@ public class MapBackedDomainResolver implements DomainResolver {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((map == null) ? 0 : map.hashCode());
+		result = prime * result + map.hashCode();
 		return result;
 	}
 
@@ -91,10 +91,7 @@ public class MapBackedDomainResolver implements DomainResolver {
 		if (getClass() != obj.getClass())
 			return false;
 		MapBackedDomainResolver other = (MapBackedDomainResolver)obj;
-		if (map == null) {
-			if (other.map != null)
-				return false;
-		} else if (!map.equals(other.map))
+		if (!map.equals(other.map))
 			return false;
 		return true;
 	}
